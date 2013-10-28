07663e49392a4f352f95022e94ea105ba7487b49
rem8086 <rem8086@gmail.com>
2013-10-28 18:37:07 +0400

    Add Tangens Operands

---
+++ ParserLoh/Operations/OperationTangens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserLoh.Operations
+{
+    class OperationTangens: IOperation
+    {
+        public int Arity
+        {
+            get { return 1; }
+        }
+
+        public double Execute(ArrayList operands)
+        {
+            return Math.Tan((double)operands[0]);
+        }
+    }
+}
